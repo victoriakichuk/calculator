663a5b6c45c6cc141d20af728ec4aa13c64758d5
Victoria Chiciuc <victoria.chiciuc@halleykm.md>
2025-12-03 14:49:43 +0200

    Working basic calculations like /,*,-,+

--- script.js
+++ script.js
@@ -1,85 +1,106 @@
-let currentValue = '';
-let previousValue = '';
-let operatorValue = '';
-let memoryValue = '';
-let result = '';
-let expression = '';
+let currentValue = "";
+let previousValue = "";
+let operatorValue = "";
+let memoryValue = "";
+let result = "";
+let expression = "";
 const num = [];
 const tokens = [];
+let finalExpression;
 
-const display = document.querySelector('.display');
-const recallOrClear = document.getElementById('recallOrClear');
-const subMemory = document.getElementById('subMemory');
-const addMemory = document.getElementById('addMemory');
-const direction = document.getElementById('direction');
-const clearEntry = document.getElementById('clearEntry');
-const allClear = document.getElementById('allClear');
-const period = document.getElementById('period');
-const equal = document.getElementById('equal');
+const display = document.querySelector(".display");
+const recallOrClear = document.getElementById("recallOrClear");
+const subMemory = document.getElementById("subMemory");
+const addMemory = document.getElementById("addMemory");
+const direction = document.getElementById("direction");
+const clearEntry = document.getElementById("clearEntry");
+const allClear = document.getElementById("allClear");
+const period = document.getElementById("period");
+const equal = document.getElementById("equal");
 
-function addNumbers (...num) {
-    let sum = 0;
-    for (let i = 0; i < num.length; i++) {
-        sum += num[i];
-    }
-    return sum;
-}
-
-document.querySelectorAll(".number").forEach(number => {
-    number.addEventListener("click", event => {
+document.querySelectorAll(".number").forEach((number) => {
+    number.addEventListener("click", (event) => {
         currentValue = currentValue + event.target.textContent;
         display.textContent = expression + currentValue;
-    })
-})
+    });
+});
 
-document.querySelectorAll(".operator").forEach(operator => {
-    operator.addEventListener("click", event => {
+document.querySelectorAll(".operator").forEach((operator) => {
+    operator.addEventListener("click", (event) => {
         previousValue = expression + currentValue;
         tokens.push(currentValue);
-        currentValue = '';
+        currentValue = "";
         operatorValue = event.target.textContent;
         tokens.push(operatorValue);
         expression = previousValue + operatorValue;
         display.textContent = expression;
-    })
-})
+    });
+});
 
-
-function mathOperations () {
-    if (operatorValue == "+") {
-        return addNumbers(num);
-    } 
-}
-
-function calculateExpression () {
+function calculateExpression() {
     let left;
     let right;
     let multRes;
     let divRes;
-    const firstOperations = tokens.find((element) => element == "X" || element == "/");
-    const index = tokens.indexOf(firstOperations);
 
-    for (let i = 0; i < tokens.length; i++) {
-        if (index !== -1) { 
-            left = tokens[index - 1];
-            right = tokens[index + 1];
-        }
-        indexLeft = tokens.indexOf(left); 
-        while (tokens.includes("X")) {
+    while (tokens.includes("X") || tokens.includes("/")) {
+        const firstOperations = tokens.find(
+            (element) => element == "X" || element == "/"
+        );
+        const index = tokens.indexOf(firstOperations);
+        if (firstOperations == "X") {
+            if (index !== -1) {
+                left = tokens[index - 1];
+                right = tokens[index + 1];
+            }
             multRes = Number(left) * Number(right);
-            tokens.splice(indexLeft, 3, multRes);
-        } 
-        while (tokens.includes("/")) {
-            divRes = Number(left) / Number(right);
-            tokens.splice(indexLeft, 3, divRes);
+            tokens.splice(index - 1, 3, multRes);
         }
-        console.log(tokens)
+
+        if (firstOperations == "/") {
+            if (index !== -1) {
+                left = tokens[index - 1];
+                right = tokens[index + 1];
+            }
+            divRes = Number(left) / Number(right);
+            tokens.splice(index - 1, 3, divRes);
+        }
+        if (tokens.length == 1) {
+            finalExpression = tokens[0];
+        }
     }
+    while (tokens.includes("+") || tokens.includes("-")) {
+        const nextOperations = tokens.find(
+            (element) => element == "+" || element == "-"
+        );
+        const index = tokens.indexOf(nextOperations);
+        if (nextOperations == "+") {
+            if (index !== -1) {
+                left = tokens[index - 1];
+                right = tokens[index + 1];
+            }
+            sum = Number(left) + Number(right);
+            tokens.splice(index - 1, 3, sum);
+        }
+
+        if (nextOperations == "-") {
+            if (index !== -1) {
+                left = tokens[index - 1];
+                right = tokens[index + 1];
+            }
+            sub = Number(left) - Number(right);
+            tokens.splice(index - 1, 3, sub);
+        }
+        if (tokens.length == 1) {
+            finalExpression = tokens[0];
+        }
+    }
+
     return tokens;
 }
 
 document.querySelector(".equal").addEventListener("click", () => {
     tokens.push(currentValue);
-    calculateExpression ();
-})
-
+    calculateExpression();
+    display.textContent = finalExpression;
+});
